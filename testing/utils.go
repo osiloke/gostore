b3dcad1e6dea9c6ac53c911082c5f924de7e5be1
Osiloke Emoekpere <osi@progwebtech.com>
2017-10-11 13:40:12 +0100

    api change related to gostore test utils

--- testing/utils.go
+++ testing/utils.go
@@ -1,5 +1,6 @@
 package testing
 
+// this package will hold a generic test methods for running tests for gostore implementations
 import (
 	"github.com/osiloke/gostore"
 	. "github.com/smartystreets/goconvey/convey"
@@ -8,8 +9,8 @@ import (
 func ConveySaveAndGet(collection string, store gostore.ObjectStore) {
 	Convey("After creating a things table", func() {
 		Convey("After inserting one row", func() {
-			item := map[string]interface{}{"name": "First Thing", "kind": "thing", "rating": 4.99}
-			key, _ := store.Save(collection, &item)
+			item := map[string]interface{}{"id": "1", "name": "First Thing", "kind": "thing", "rating": 4.99}
+			key, _ := store.Save("1", collection, &item)
 			Convey("The stored data is retrieved", func() {
 				var storedItem map[string]interface{}
 				store.Get(key, collection, &storedItem)
